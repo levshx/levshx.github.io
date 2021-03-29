7f04d68b38950e0053a84069fab8c7b87a8d85d9
Pavel Levshic <pavellevshic@gmail.com>
2021-03-29 20:54:17 +0300

    Create routing.js

---
+++ src/js/routing.js
@@ -0,0 +1,16 @@
+var app = angular.module("app", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "pages/main.htm"
+    })
+    .when("/projects", {
+        templateUrl : "pages/projects.htm"
+    })
+    .when("/contact", {
+        templateUrl : "pages/contact.htm"
+    })
+    .otherwise({
+        template: "<h1>Page not found<h1><br><h2>Error 404<h2>"
+    })
+});
