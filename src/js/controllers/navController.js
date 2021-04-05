9297b1b7fda3b689d5a362de6ac6204a6928ba94
Pavel Levshic <pavellevshic@gmail.com>
2021-04-05 21:53:45 +0300

    Add files via upload

---
+++ src/js/controllers/navController.js
@@ -0,0 +1,7 @@
+app.controller('navController', ['$scope', '$location', function($scope, $location){
+
+	$scope.isActive = function(destination){
+		return destination === $location.path();
+	}
+
+}]);
