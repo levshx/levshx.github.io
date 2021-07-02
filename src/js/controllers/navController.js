a26008909e025226e1484c89a9ab151735f998d2
Pavel Levshic <pavellevshic@gmail.com>
2021-07-02 16:10:33 +0300

    Update navController.js

--- src/js/controllers/navController.js
+++ src/js/controllers/navController.js
@@ -1,9 +1,1 @@
-app.controller('navController', ['$scope', '$location', function($scope, $location){
 
-	$scope.isActive = function(destination){
-		return destination === $location.path();
-	}
-	
-	console.log($location.path());
-
-}]);
