4b7300d5feb08f79cefd236965af4463ac60f880
Pavel Levshic <pavellevshic@gmail.com>
2022-03-28 11:03:24 +0300

    Update main.3d.js

--- projects/elevator/main.3d.js
+++ projects/elevator/main.3d.js
@@ -41,6 +41,89 @@ function init() {
 	const directionalLight = new THREE.DirectionalLight(0xffffff, 0.1);
 	scene.add(directionalLight);
 
+	//TEXT
+	const loader = new FontLoader();
+	loader.load( 'helvetiker_regular.typeface.json', function ( font ) {
+
+		const color = 0x006699;
+
+		const matDark = new THREE.LineBasicMaterial( {
+			color: color,
+			side: THREE.DoubleSide
+		} );
+
+		const matLite = new THREE.MeshBasicMaterial( {
+			color: color,
+			transparent: true,
+			opacity: 0.4,
+			side: THREE.DoubleSide
+		} );
+
+		const message = '   Этаж\n1';
+
+		const shapes = font.generateShapes( message, 100 );
+
+		const geometry = new THREE.ShapeGeometry( shapes );
+
+		geometry.computeBoundingBox();
+
+		const xMid = - 0.5 * ( geometry.boundingBox.max.x - geometry.boundingBox.min.x );
+
+		geometry.translate( xMid, 0, 0 );
+
+		// make shape ( N.B. edge view not visible )
+
+		const text = new THREE.Mesh( geometry, matLite );
+		text.position.z = - 150;
+		scene.add( text );
+
+		// make line shape ( N.B. edge view remains visible )
+
+		const holeShapes = [];
+
+		for ( let i = 0; i < shapes.length; i ++ ) {
+
+			const shape = shapes[ i ];
+
+			if ( shape.holes && shape.holes.length > 0 ) {
+
+				for ( let j = 0; j < shape.holes.length; j ++ ) {
+
+					const hole = shape.holes[ j ];
+					holeShapes.push( hole );
+
+				}
+
+			}
+
+		}
+
+		shapes.push.apply( shapes, holeShapes );
+
+		const lineText = new THREE.Object3D();
+
+		for ( let i = 0; i < shapes.length; i ++ ) {
+
+			const shape = shapes[ i ];
+
+			const points = shape.getPoints();
+			const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+			geometry.translate( xMid, 0, 0 );
+
+			const lineMesh = new THREE.Line( geometry, matDark );
+			lineText.add( lineMesh );
+
+		}
+
+		scene.add( lineText );
+
+		render();
+
+	} ); //end load function
+	
+	
+	
 	// model
 
 	new GLTFLoader().load('bot.gltf', function (gltf) {
