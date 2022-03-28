0e1320cf3d709f9a7d7603c3042c08e6a590f262
Pavel Levshic <pavellevshic@gmail.com>
2022-03-28 12:25:57 +0300

    Update main.3d.js

--- projects/elevator/main.3d.js
+++ projects/elevator/main.3d.js
@@ -43,7 +43,7 @@ function init() {
 
 	//TEXT
 	const loader = new FontLoader();
-	loader.load( 'Monospace.font.json', function ( font ) {
+	loader.load( 'OpenSans.facetype.json', function ( font ) {
 
 		const color = 0x006699;
 
