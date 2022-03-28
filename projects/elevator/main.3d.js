54054a2368a179e3c17923f79ebcdbf5e8e99b05
Pavel Levshic <pavellevshic@gmail.com>
2022-03-28 11:45:18 +0300

    Update main.3d.js

--- projects/elevator/main.3d.js
+++ projects/elevator/main.3d.js
@@ -74,52 +74,8 @@ function init() {
 		// make shape ( N.B. edge view not visible )
 
 		const text = new THREE.Mesh( geometry, matLite );
-		text.position.z = - 150;
+		text.position.z = 0;
 		scene.add( text );
-
-		// make line shape ( N.B. edge view remains visible )
-
-		const holeShapes = [];
-
-		for ( let i = 0; i < shapes.length; i ++ ) {
-
-			const shape = shapes[ i ];
-
-			if ( shape.holes && shape.holes.length > 0 ) {
-
-				for ( let j = 0; j < shape.holes.length; j ++ ) {
-
-					const hole = shape.holes[ j ];
-					holeShapes.push( hole );
-
-				}
-
-			}
-
-		}
-
-		shapes.push.apply( shapes, holeShapes );
-
-		const lineText = new THREE.Object3D();
-
-		for ( let i = 0; i < shapes.length; i ++ ) {
-
-			const shape = shapes[ i ];
-
-			const points = shape.getPoints();
-			const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-			geometry.translate( xMid, 0, 0 );
-
-			const lineMesh = new THREE.Line( geometry, matDark );
-			lineText.add( lineMesh );
-
-		}
-
-		scene.add( lineText );
-
-		render();
-
 	} ); //end load function
 	
 	
